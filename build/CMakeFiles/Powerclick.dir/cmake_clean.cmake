8b9f5bc0d6a11a2deeca146a994e9c21794e1386
Chen Yu <yuchen2501@csu.fullerton.edu>
2026-02-25 13:54:01 -0800

    build and finished main.cpp file

---
+++ build/CMakeFiles/Powerclick.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Powerclick.dir/src/main.cpp.o"
+  "CMakeFiles/Powerclick.dir/src/main.cpp.o.d"
+  "Powerclick"
+  "Powerclick.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Powerclick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
