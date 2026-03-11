79a0a1fe41827db974c9f6365279ff1475f47679
Chen Yu <yuchen2501@csu.fullerton.edu>
2026-03-10 22:30:01 -0700

    build house.cpp file and finished constructor part

---
+++ src/House.cpp
@@ -0,0 +1,17 @@
+#include "House.hpp"
+#include "Constant.hpp"
+
+//constructor
+House::House() {
+    x = HOUSE_X;
+    y = HOUSE_Y;
+    width = HOUSE_WIDTH;
+    height = HOUSE_HEIGHT;
+
+    shape.setSize(sf::Vector2f(width, height));
+    shape.setPosition(sf::Vector2f(x, y));
+    shape.setFillColor(sf::Color(70, 130, 180));
+    shape.setOutLineColor(sf::Color::white);
+    shape.setOutLineThickness(2.0f);
+}
+
