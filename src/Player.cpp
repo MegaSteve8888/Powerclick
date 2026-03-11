e761553958885e94be60ef6971b9b9b1c14f2566
Chen Yu <yuchen2501@csu.fullerton.edu>
2026-03-10 21:26:40 -0700

    finished points and get score function

--- src/Player.cpp
+++ src/Player.cpp
@@ -6,3 +6,13 @@ Player::player() {
     score = STRARTING_SCORE;
     lives = PLAYER_LIVES;
 }
+
+//add points when player click the enemy
+void Player::addScore(int points) {
+    score += points;
+}
+
+//get current score
+int Player::getScore() const {
+    return score;
+}
