beebd4ed726b909e1b34f8ce2bae18a9fe4398fa
Chen Yu <yuchen2501@csu.fullerton.edu>
2026-03-24 13:21:28 -0700

    build audiomanager.cpp file and finihsed audiomanager function and init function

--- src/AudioManager.cpp
+++ src/AudioManager.cpp
@@ -4,4 +4,24 @@ AudioManager::AudioManager() {
     soudEnabled = true;
 }
 
+bool AudioManager::int() {
+    bool success = true;
+    if (!clickBuffer.laodFromFile("assets/sounds/click.wav")) {
+        success = false;
+    } else {
+        clickSound.setBuffer(clickBuffer);
+    }
+
+    if (!hitBuffer.loadFromFile("assets/sounds/hit.wav")) {
+        success = false;
+    } else {
+        hitSound.setBuffer(hitBuffer);
+    }
+    if (!gmaeOverBuffer.loadFromFile("assets/sounds/gameover.wav")) {
+        success = false;
+    } else {
+        gameOverSound.setBuffer(gameOverBuffer);
+    }
+    return success;
+}
 
