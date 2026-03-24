203f60826e6069acf2cf0be27c46becd32f996a5
Chen Yu <yuchen2501@csu.fullerton.edu>
2026-03-24 13:38:42 -0700

    move the position of AudioManager file

--- build/CMakeFiles/Powerclick.dir/cmake_clean.cmake
+++ build/CMakeFiles/Powerclick.dir/cmake_clean.cmake
@@ -1,8 +1,12 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/Powerclick.dir/src/AudioManager.cpp.o"
+  "CMakeFiles/Powerclick.dir/src/AudioManager.cpp.o.d"
   "CMakeFiles/Powerclick.dir/src/House.cpp.o"
   "CMakeFiles/Powerclick.dir/src/House.cpp.o.d"
   "CMakeFiles/Powerclick.dir/src/Player.cpp.o"
   "CMakeFiles/Powerclick.dir/src/Player.cpp.o.d"
+  "CMakeFiles/Powerclick.dir/src/Utilities.cpp.o"
+  "CMakeFiles/Powerclick.dir/src/Utilities.cpp.o.d"
   "CMakeFiles/Powerclick.dir/src/main.cpp.o"
   "CMakeFiles/Powerclick.dir/src/main.cpp.o.d"
   "Powerclick"
