4d93800e7519dede4f773f05c4e6ca40176bd8dc
sangydoan <sangydoan@gmail.com>
2026-04-15 13:24:34 -0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,5 +1,16 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include "Enemy.h"
+#include "UI.h"
+
+// Game states
+enum class GameState
+{
+    Menu,
+    Playing,
+    GameOver
+};
 
 class Game
 {
@@ -12,15 +23,25 @@ private:
     void update(float dt);
     void render();
 
-    // Event helpers
     void handleKeyPressed(sf::Keyboard::Key key);
     void handleMousePressed(sf::Mouse::Button button, int x, int y);
     void handleMouseMoved(int x, int y);
 
+    void spawnEnemy();
+
 private:
     sf::RenderWindow m_window;
 
-    // Example state (stub for later assignments)
+    GameState m_state = GameState::Menu;
+
+    std::vector<Enemy> m_enemies;
+    UI m_ui;
+
+    int m_score = 0;
+    int m_lives = 3;
+
+    float m_spawnTimer = 0.f;
+
     bool m_paused = false;
     sf::Vector2i m_mousePos{0, 0};
 };
