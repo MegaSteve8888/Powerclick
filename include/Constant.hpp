66f11359a6f8ed231903fb8b60186cec3f2004df
Chen Yu <yuchen2501@csu.fullerton.edu>
2026-04-15 08:37:14 -0700

    fix window size and adjust house position

--- include/Constant.hpp
+++ include/Constant.hpp
@@ -2,8 +2,8 @@
 #define CONSTANT_HPP
 
 //setting game window
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 800;
 const char GAME_TITLE[] = "Powerclick";
 const int FRAME_RATE = 60;
 
@@ -14,7 +14,7 @@ const int STRARTING_SCORE = 0;
 //setting house
 const float HOUSE_WIDTH = 120.0f;
 const float HOUSE_HEIGHT = 100.0f;
-const float HOUSE_X = (SCREEN_WIDTH - HOUSE_WIDTH) / 2.0f;
+const float HOUSE_X = SCREEN_WIDTH - HOUSE_WIDTH - 140.0f;
 const float HOUSE_Y = (SCREEN_HEIGHT - HOUSE_HEIGHT) / 2.0f;
 
 // setting game enemy
